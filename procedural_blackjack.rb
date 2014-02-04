c752969fa1a5a760bc168dc868601908ef93edd5
nick.robertson <nick.robertson@octanner.com>
2014-02-04 13:03:39 -0700

    Completed Blackjack game

--- procedural_blackjack.rb
+++ procedural_blackjack.rb
@@ -58,5 +58,77 @@ puts ""
 puts "The dealers has the #{dealer_hand[0][0]} of #{dealer_hand[0][1]},
 and the #{dealer_hand[1][0]} of #{dealer_hand[1][1]}, for a total of #{dealer_total}."
 puts ""
+
+#player turn
+if player_total == 21
+  puts "Congrats, you hit Blackjack, you win!!!"
+  exit
+end
+while player_total < 21
 puts "What would you like to do? 1) hit 2) stay"
 hit_or_stay = gets.chomp
+  if !['1', '2'].include?(hit_or_stay)
+    puts "Error: you must enter 1 or 2"
+    next
+  end
+
+  if hit_or_stay == '2'
+    break
+  end
+
+  new_card = arr_deck.pop
+  puts "Dealing card to player: #{new_card}"
+  player_hand << new_card
+  player_total = calculate_total(player_hand)
+  puts "Your total is now: #{player_total}"
+
+  if player_total == 21
+    puts "Congrats, you hit Blackjack, you win!!!"
+    exit
+  elsif player_total > 21
+    puts "Sorry, you busted!"
+    exit
+  end
+end
+
+#Dealer turn
+if dealer_total == 21
+  puts "Sorry, the dealer hit Blackjack"
+  exit
+end
+
+while dealer_total < 17
+  #hit
+  new_card = arr_deck.pop
+  puts "Dealing new card for dealer: #{new_card}"
+  dealer_hand << new_card
+  dealer_total = calculate_total(dealer_hand)
+  puts "Dealer total is now #{dealer_total}"
+
+  if dealer_total == 21
+    puts "Sorry, dealer hit black. You lose!"
+    exit
+  elsif dealer_total > 21
+    puts "Congrats the dealer busted. You win!"
+    exit
+  end
+end
+
+puts "Dealer's cards: "
+dealer_hand.each do |card|
+  puts "=> #{card}"
+end
+puts ""
+puts "Player cards: "
+player_hand.each do |card|
+  puts "=> #{card}"
+end
+
+if dealer_total > player_total
+  puts "Sorry, the dealer won!"
+elsif dealer_total < player_total
+  puts "Congrats you win!"
+else
+  puts "It is a tie!"
+  exit
+end
